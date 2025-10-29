e28cde62d790e127f12b6ffdbb1d9997789626fe
TrailingWhitespace <flash1719@protonmail.com>
2025-10-29 19:33:50 +0000

    added some inputs

--- flake.nix
+++ flake.nix
@@ -1,3 +1,30 @@
 {
   description = "My NixOS configuration";
+
+  inputs = {
+   
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # Impermanence
+    # Sops
+    # spicetify
+    # winboat
+    # quickshell / dms
+
+    
+ 
+  };
+
+  
 }
